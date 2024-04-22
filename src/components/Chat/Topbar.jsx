96181b42188ccad31c2ff97b439d42bc81471c44
tallen102 <allen102@gannon.edu>
2024-04-22 08:48:28 -0400

    update Chatbox layout

--- src/components/Chat/Topbar.jsx
+++ src/components/Chat/Topbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const TopBar = () => {
   return (
-    <Box borderWidth="1px" borderRadius="md" p="10" mb="0" width="100%">
+    <Box borderWidth="1px" borderRadius="md" p="7" mb="0" width="100%">
       <Heading as="h1" size="lg" >
         Messages
       </Heading>
